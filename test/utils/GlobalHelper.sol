6557ad0ee306b169603507b501fde560e0264326
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-10-26 13:21:35 +0530

    test: Write the global helper and also include a mock ERC20 contract
    The mock ERC20 allows anyone to mint any amount of tokens

--- test/utils/GlobalHelper.sol
+++ test/utils/GlobalHelper.sol
@@ -3,6 +3,30 @@ pragma solidity 0.8.24;
 
 import { Test } from "forge-std/Test.sol";
 
+import { Stream } from "../../src/Stream.sol";
+import { MockERC20 } from "./MockERC20.sol";
+
 contract GlobalHelper is Test {
-    function setUp() public { }
+    address public owner;
+    Stream public stream;
+
+    address public streamer;
+    address public recipient;
+
+    MockERC20 public token;
+
+    function setUp() public {
+        owner = makeAddr("owner");
+
+        vm.startPrank(owner);
+        stream = new Stream(payable(owner));
+        vm.stopPrank();
+
+        streamer = makeAddr("streamer");
+        recipient = makeAddr("recipient");
+
+        string memory tokenName = "Mock ERC20";
+        string memory tokenSymbol = "MERC20";
+        token = new MockERC20(tokenName, tokenSymbol);
+    }
 }
