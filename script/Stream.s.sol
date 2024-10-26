28033a7fa9b8980c0aec4b4232dc753cea9f076a
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-10-26 13:20:10 +0530

    feat: Write a deployment script
    Also include a local deployment command in the Makefile

--- script/Stream.s.sol
+++ script/Stream.s.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.8.13;
 
 import { Script } from "forge-std/Script.sol";
 
+import { Stream } from "../src/Stream.sol";
+
 contract StreamScript is Script {
+    Stream public stream;
+
     function run() public {
         vm.startBroadcast();
+        stream = new Stream(payable(msg.sender));
         vm.stopBroadcast();
     }
 }
