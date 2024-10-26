3cf28d6e513580c83a58d96d16cbb1b62baaf12f
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-10-26 15:30:16 +0530

    test: Finish testing allowStream() and stream() functions

--- test/Stream.t.sol
+++ test/Stream.t.sol
@@ -1,6 +1,240 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
 import { GlobalHelper } from "./utils/GlobalHelper.sol";
 
-contract StreamTest is GlobalHelper { }
+contract StreamInitializationTest is GlobalHelper {
+    uint256 public amountToStream = 100e18;
+    uint256 public window = 1 minutes;
+
+    function test_computeHash() public view {
+        bytes32 expectedHash = keccak256(abi.encodePacked(streamer, address(token), recipient));
+
+        assertEq(stream.computeHash(streamer, address(token), recipient), expectedHash);
+    }
+
+    function test_createNewStream() public {
+        bool streamOnce = true;
+
+        bytes32 streamHash = _createStream(streamOnce);
+
+        (
+            address actualStreamer,
+            address actualRecipient,
+            address actualToken,
+            uint256 totalStreamed,
+            uint256 outstanding,
+            uint256 allowable,
+            uint256 actualWindow,
+            uint256 timestamp,
+            bool once
+        ) = stream.streamDetails(streamHash);
+        bytes32[] memory streamDetailsByStreamer = stream.viewStreamerAllowances(streamer);
+        bytes32[] memory streamDetailsByRecipient = stream.viewRecipientAllowances(recipient);
+
+        assertEq(actualStreamer, streamer);
+        assertEq(actualRecipient, recipient);
+        assertEq(actualToken, address(token));
+        assertEq(totalStreamed, 0);
+        assertEq(outstanding, amountToStream);
+        assertEq(allowable, amountToStream);
+        assertEq(actualWindow, window);
+        assertEq(timestamp, block.timestamp);
+        assertEq(once, streamOnce);
+
+        assertEq(streamDetailsByStreamer.length, 1);
+        assertEq(streamDetailsByRecipient.length, 1);
+        assertEq(streamDetailsByStreamer[0], streamHash);
+        assertEq(streamDetailsByRecipient[0], streamHash);
+    }
+
+    function test_overwriteStream() public {
+        bool streamOnce = true;
+
+        bytes32 streamHash = _createStream(streamOnce);
+
+        uint256 newAmountToStream = 50e18;
+        uint256 newWindow = 2 minutes;
+        bool newStreamOnceFlag = !streamOnce;
+
+        vm.startPrank(streamer);
+        stream.allowStream(address(token), recipient, newAmountToStream, newWindow, newStreamOnceFlag);
+        vm.stopPrank();
+
+        (,,,, uint256 outstanding, uint256 allowable, uint256 actualWindow, uint256 timestamp, bool once) =
+            stream.streamDetails(streamHash);
+
+        assertEq(outstanding, newAmountToStream);
+        assertEq(allowable, newAmountToStream);
+        assertEq(actualWindow, newWindow);
+        assertEq(timestamp, block.timestamp);
+        assertEq(once, newStreamOnceFlag);
+    }
+
+    function test_creatingAStreamEmitsEvent() public {
+        bool streamOnce = true;
+
+        vm.startPrank(streamer);
+        vm.expectEmit(true, true, true, true);
+        emit StreamAllowed(streamer, address(token), recipient, amountToStream);
+        stream.allowStream(address(token), recipient, amountToStream, window, streamOnce);
+        vm.stopPrank();
+    }
+
+    function test_collectingFundsFromANonExistentStreamFails() public {
+        vm.expectRevert(bytes(STREAM_DOES_NOT_EXIST));
+        stream.stream(address(token), owner, recipient);
+    }
+
+    function test_collectingZeroFundsFromStreamFails() public {
+        bool streamOnce = true;
+
+        _createStream(streamOnce);
+
+        vm.expectRevert(bytes(NO_ALLOWABLE_AMOUNT_TO_WITHDRAW));
+        stream.stream(address(token), streamer, recipient);
+    }
+
+    function test_collectFundsFromStreamOnceHalfWayThroughWindow() public {
+        bool streamOnce = true;
+
+        bytes32 streamHash = _createStream(streamOnce);
+        _mintTokensToStreamerAndProvideAllowanceToStream(amountToStream);
+
+        vm.warp(block.timestamp + window / 2);
+
+        stream.stream(address(token), streamer, recipient);
+
+        (,,, uint256 totalStreamed, uint256 outstanding,,, uint256 timestamp,) = stream.streamDetails(streamHash);
+
+        assertEq(token.balanceOf(streamer), amountToStream / 2);
+        assertEq(token.balanceOf(recipient), amountToStream / 2);
+
+        assertEq(totalStreamed, amountToStream / 2);
+        assertEq(outstanding, amountToStream / 2);
+        assertEq(timestamp, block.timestamp);
+    }
+
+    function test_collectAllFundsFromStreamOnce() public {
+        bool streamOnce = true;
+
+        bytes32 streamHash = _createStream(streamOnce);
+        _mintTokensToStreamerAndProvideAllowanceToStream(amountToStream);
+
+        vm.warp(block.timestamp + window);
+
+        stream.stream(address(token), streamer, recipient);
+
+        (,,, uint256 totalStreamed, uint256 outstanding,,, uint256 timestamp,) = stream.streamDetails(streamHash);
+
+        assertEq(token.balanceOf(streamer), 0);
+        assertEq(token.balanceOf(recipient), amountToStream);
+
+        assertEq(totalStreamed, amountToStream);
+        assertEq(outstanding, 0);
+        assertEq(timestamp, block.timestamp);
+    }
+
+    function test_collectAllFundsFromStreamOnceWayPastWindow() public {
+        bool streamOnce = true;
+
+        bytes32 streamHash = _createStream(streamOnce);
+        _mintTokensToStreamerAndProvideAllowanceToStream(amountToStream);
+
+        vm.warp(block.timestamp + window + 1 minutes);
+
+        stream.stream(address(token), streamer, recipient);
+
+        (,,, uint256 totalStreamed, uint256 outstanding,,, uint256 timestamp,) = stream.streamDetails(streamHash);
+
+        assertEq(token.balanceOf(streamer), 0);
+        assertEq(token.balanceOf(recipient), amountToStream);
+
+        assertEq(totalStreamed, amountToStream);
+        assertEq(outstanding, 0);
+        assertEq(timestamp, block.timestamp);
+    }
+
+    function test_collectFundsFromRecurringStream() public {
+        bool streamOnce = false;
+
+        bytes32 streamHash = _createStream(streamOnce);
+        _mintTokensToStreamerAndProvideAllowanceToStream(amountToStream);
+
+        vm.warp(block.timestamp + window);
+
+        stream.stream(address(token), streamer, recipient);
+
+        (,,, uint256 totalStreamed, uint256 outstanding,,, uint256 timestamp,) = stream.streamDetails(streamHash);
+
+        assertEq(token.balanceOf(streamer), 0);
+        assertEq(token.balanceOf(recipient), amountToStream);
+
+        assertEq(totalStreamed, amountToStream);
+        assertEq(outstanding, amountToStream);
+        assertEq(timestamp, block.timestamp);
+
+        _mintTokensToStreamerAndProvideAllowanceToStream(amountToStream);
+
+        vm.warp(block.timestamp + window);
+
+        stream.stream(address(token), streamer, recipient);
+
+        (,,, totalStreamed, outstanding,,, timestamp,) = stream.streamDetails(streamHash);
+
+        assertEq(token.balanceOf(streamer), 0);
+        assertEq(token.balanceOf(recipient), amountToStream * 2);
+
+        assertEq(totalStreamed, amountToStream * 2);
+        assertEq(outstanding, amountToStream);
+        assertEq(timestamp, block.timestamp);
+    }
+
+    function test_collectFundsFromRecurringStreamWayPastWindow() public {
+        bool streamOnce = false;
+
+        bytes32 streamHash = _createStream(streamOnce);
+        _mintTokensToStreamerAndProvideAllowanceToStream(amountToStream * 2);
+
+        vm.warp(block.timestamp + window * 2);
+
+        stream.stream(address(token), streamer, recipient);
+
+        (,,, uint256 totalStreamed, uint256 outstanding,,, uint256 timestamp,) = stream.streamDetails(streamHash);
+
+        assertEq(token.balanceOf(streamer), 0);
+        assertEq(token.balanceOf(recipient), amountToStream * 2);
+
+        assertEq(totalStreamed, amountToStream * 2);
+        assertEq(outstanding, amountToStream);
+        assertEq(timestamp, block.timestamp);
+    }
+
+    function test_collectingFundsFromStreamEmitsEvent() public {
+        bool streamOnce = false;
+
+        _createStream(streamOnce);
+        _mintTokensToStreamerAndProvideAllowanceToStream(amountToStream);
+
+        vm.warp(block.timestamp + window);
+
+        vm.expectEmit(true, true, true, true);
+        emit Streamed(address(token), streamer, recipient, amountToStream);
+        stream.stream(address(token), streamer, recipient);
+    }
+
+    function _createStream(bool _streamOnce) internal returns (bytes32 streamHash) {
+        vm.startPrank(streamer);
+        stream.allowStream(address(token), recipient, amountToStream, window, _streamOnce);
+        vm.stopPrank();
+
+        streamHash = stream.computeHash(streamer, address(token), recipient);
+    }
+
+    function _mintTokensToStreamerAndProvideAllowanceToStream(uint256 _amount) internal {
+        vm.startPrank(streamer);
+        token.mint(streamer, _amount);
+        token.approve(address(stream), _amount);
+        vm.stopPrank();
+    }
+}
